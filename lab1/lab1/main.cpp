2fffe4ae11f11175af9900fafd7b9915d90767e6
yarentag <yarentag23@gmail.com>
2023-10-15 20:38:01 +0300

    main fonksiyon yazıldı ve input.txt eklendi

--- lab1/lab1/main.cpp
+++ lab1/lab1/main.cpp
@@ -34,3 +34,36 @@ int findSmallest(const int numbers[], int count) {
     }
     return smallest;
 }
+
+
+int main() {
+    ifstream inputFile("input.txt");
+
+    if (!inputFile.is_open()) {
+        // Use cout for error messages
+        cout << "Error opening input file!" << endl;
+        return 1;
+    }
+
+    int count;
+    inputFile >> count;
+
+    int* numbers = new int[count];
+
+    for (int i = 0; i < count; ++i) {
+        inputFile >> numbers[i];
+    }
+
+    inputFile.close();
+
+    // Calculate and display results
+    cout << "Sum: " << calculateSum(numbers, count) << endl;
+    cout << "Product: " << calculateProduct(numbers, count) << endl;
+    cout << "Average: " << calculateAverage(numbers, count) << endl;
+    cout << "Smallest: " << findSmallest(numbers, count) << endl;
+
+    // Don't forget to release memory allocated with 'new'
+    delete[] numbers;
+
+    return 0;
+}
